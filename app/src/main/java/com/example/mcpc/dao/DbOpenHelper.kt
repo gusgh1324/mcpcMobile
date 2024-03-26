cbf1178e8ecf6d33b1281228bf2b9620407698ea
sko3on <tmdgns0311@naver.com>
2024-03-26 15:02:07 +0900

    Regist edit 부분 추가 필요

--- app/src/main/java/com/example/mcpc/dao/DbOpenHelper.kt
+++ app/src/main/java/com/example/mcpc/dao/DbOpenHelper.kt
@@ -8,15 +8,25 @@ import android.database.sqlite.SQLiteOpenHelper
 class DbOpenHelper(
     ctx: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, ver: Int
 ): SQLiteOpenHelper(ctx, name, factory, ver){
+
     override fun onCreate(db: SQLiteDatabase?) {
+        /*user 테이블*/
         db?.execSQL("create table if not exists user " +
-                "(_num integer primary key autoincrement,id text, pw text, name text, " +
-                "age integer, mobile text)")
+                "(_uNum integer primary key autoincrement,uId text, uPw text, uName text, " +
+                "uage integer, uMobile text)")
+//        /*store 테이블*/
+//        db?.execSQL("create table if not exists store " +
+//                "(_sNum integer primary key autoincrement,sId text, sPw text, sName text, " +
+//                "sMobile text)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         if (oldVersion != newVersion){
             db?.execSQL("drop table if exists user")
+            onCreate(db)
+        }
+        if (oldVersion != newVersion){
+            db?.execSQL("drop table if exists store")
             onCreate(db)
         }
     }
